867980437c10ad8a294539c5376ed38a13c69f1a
samuelito22 <sedorodion@hotmail.com>
2022-10-18 11:51:14 +0100

    main

--- source/src/Components/Navbar/Navbar.js
+++ source/src/Components/Navbar/Navbar.js
@@ -6,41 +6,40 @@ import {IoIosArrowUp} from 'react-icons/io'
 function Navbar() {
     const navigationLinks = [{
         mal_id: 0,
-        top: "WORK",
-        dropdown:["Secondary","Secondary","Secondary"]
-    },{
+        top: "HOME",
+        dropdown: [null]
+        },{
         mal_id: 1,
-        top: "ABOUT",
-        dropdown:["Secondary","Secondary","Secondary"]
-    },{
+        top: "ABOUT US",
+        dropdown: [null]
+        },{
         mal_id: 2,
         top: "SERVICES",
-        dropdown:["Secondary","Secondary","Secondary"]
-    },{
-        mal_id: 3,
-        top: "BLOG",
-        dropdown:["Secondary","Secondary","Secondary"]
-    },{
+        dropdown: [null]
+        },{
         mal_id: 4,
-        top: "CONTACT",
-        dropdown:["Secondary","Secondary","Secondary"]
-    }]
+        top: "CONTACT US",
+        dropdown: [null]
+        }]
 
   return (
     <div className='navbar'>
         <div className='container'>
-            <Link to='/' className='logo'><span style={{color:"green"}}>.</span>WebM</Link>
+            <Link to='/' className='logo'><span style={{color:"var(--default)"}}>.</span>WebM</Link>
             <div className='navigation'>
                 {navigationLinks.map( nav => (
                     <div key={nav.mal_id} className = "link">                        
-                    <Link to="#" className='linkPrimary'>{nav.top}<IoIosArrowUp className='arrow' /></Link>
-                    <div className='dropdown'>
+                    <Link to="#" className='linkPrimary'>{nav.top}<IoIosArrowUp className='arrow' style={{display:"none"}}/></Link>
+                    {/*<div className='dropdown'>
                         {nav.dropdown.map( drop => (
                             <Link to='#' className='linkSecondary'>{drop}</Link>
                         ))}
-                    </div>
+                        </div>*/}
                     </div>
                 ))}
+                <div className = "link">                        
+                    <Link to="#" className='btn-hireUs'>HIRE US</Link>
+                </div>
             </div>
         </div>
     </div>
--- source/src/Pages/Home/Home.js
+++ source/src/Pages/Home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='banner'>
+      <div className='container'>
+        
+      </div>
+    </div>
   )
 }
 
